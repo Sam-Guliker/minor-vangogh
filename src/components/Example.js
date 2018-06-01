d14b1a156ef9937a49e69afc9ba45c0bca3ebe38
Fenna <fennadewilde@hotmail.com>
2018-06-01 11:58:58 +0200

    added component

---
+++ src/components/Example.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Notification extends Component {
+    componentWillMount() {
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    handleKeyDown = (e) => {
+        if(e.keyCode === 32) {
+            console.log("key pressed")
+        }
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Notification;
--- src/index.js
+++ src/index.js
@@ -20,4 +20,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
 registerServiceWorker();
