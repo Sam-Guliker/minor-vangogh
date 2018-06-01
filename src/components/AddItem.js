d473a0f068f4ce48e0c261c06ee55fa5ecb55c6f
Sam-Guliker <samguliker@hotmail.com>
2018-06-01 11:57:16 +0200

    minor updates

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,14 +2,9 @@ import React, { Component } from "react";
 import ItemService from "./ItemService";
 
 class AddItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-    this.addItemService = new ItemService();
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  state = {
+    value: ""
+  };
 
   handleChange(event) {
     this.setState({ value: event.target.value });
--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 import App from "./App";
-import AddItem from "./components/AddItem";
-import IndexItem from "./components/IndexItem";
-import EditItem from "./components/EditItem";
 
-ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/add-item" component={AddItem} />
-      <Route path="/index" component={IndexItem} />
-      <Route path="/edit/:id" component={EditItem} />
-    </div>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
