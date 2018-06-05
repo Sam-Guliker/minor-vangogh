1b507f1f7bb5408496a37cf200a6f5474cff7f62
Fenna <fennadewilde@hotmail.com>
2018-06-05 14:00:43 +0200

    cleaned backend

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+       <h1>React app</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
