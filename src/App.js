49d964051cdee7df454ea2b06cb5cdaa6494998b
Fenna <fennadewilde@hotmail.com>
2018-06-25 10:21:25 +0200

    transitions

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const childFactoryCreator = classNames => child =>
 
 class App extends Component {
   render() {
-    console.log(this.props.location);
     return (
       <Route
         render={({ location }) => (
